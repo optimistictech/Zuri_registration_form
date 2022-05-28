1812a8c78ab5523ecc2ac466ac4dfe433d78c401
Samson Olamide <olaorosamson3@gmail.com>
2022-05-29 00:26:52 +0100

    Update placeholder

---
+++ registration.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+<body>
+<form method="POST" action="user_data.php">
+Name: <input type="text" name="name" placeholder="Enter your name"><br>
+Email: <input type="text" name="email" placeholder="Enter your Email"><br>
+Date of Birth : <input type="date" name="dob" placeholder="Enter your Date of Birth"><br>
+Gender: <input type="text" name="gender" placeholder="Enter your Gender"><br>
+Country: <input type="text" name="country" placeholder="Enter your Country"><br>
+<button type="submit" name="submit">Submit </button>
+</form>
+
+
+    
+</body>
+</html>
